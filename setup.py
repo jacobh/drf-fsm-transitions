4763385312a2d7f2a4c7dfc7929db5bc74fee1ff
Jacob Haslehurst <jacob@haslehurst.net>
2014-09-21 13:30:51 +0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='drf-fsm-transitions',
+    version='0.1.0',
+    description='Automatically hook your Django-FSM transitions up to Django REST Framework',
+    author='Jacob Haslehurst',
+    author_email='jacob@haslehurst.net',
+    url='https://github.com/hzy/drf-fsm-transitions',
+    packages=find_packages(),
+    install_requires=['django', 'django_fsm', 'djangorestframework']
+)
